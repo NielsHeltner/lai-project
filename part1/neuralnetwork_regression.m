a0a18ac6a5dac65ba01cef5d8d62f469b4f9c59a
Niels Heltner <niels.heltner@hotmail.com>
2019-03-12 11:46:51 +0100

    network working for formula1

--- part1/neuralnetwork_regression.m
+++ part1/neuralnetwork_regression.m
@@ -1,9 +1,41 @@
-[x, t] = simplefit_dataset;
+[x, t] = generate_inputs_and_targets(1000, -1, 1);
+%[x, t] = simplefit_dataset;
+%disp(x);
+%disp(t);
 
-net = fitnet([10, 5]);
+neuralnet = fitnet(8);
+%view(neuralnet)
 
-%view(net)
+neuralnet = train(neuralnet, x, t);
 
-net = train(net, x, t);
+%view(neuralnet)
 
-view(net)
+
+function [x, t] = generate_inputs_and_targets(amount, min, max)
+    %x = zeros(1, amount);
+    %t = zeros(1, amount);
+    for i = 1:amount
+        number = (max - min) * rand + min;
+        x(i) = number;
+        t(i) = formula1(number);
+    end
+end
+
+function [x, t] = generate_inputs_and_targets2(amount, min, max)
+    %x = zeros(1, amount);
+    %t = zeros(1, amount);
+    for i = 1:amount
+        number = (max - min) * rand + min;
+        number1 = (max - min) * rand + min;
+        x(i) = [number, number1];
+        t(i) = formula2(number, number1);
+    end
+end
+
+function y = formula1(x)
+    y = sin(2 * pi * x) + sin(5 * pi * x);
+end
+
+function z = formula2(x, y)
+    z = exp(-(x^2 + y^2) / 0.1);
+end
